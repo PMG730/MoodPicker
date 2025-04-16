42f129f887b21f914a61c2203f29a90aac8e1fc2
GREER <pg0964202@dt050624.otc.edu>
2025-04-16 09:04:51 -0500

    init commit

---
+++ MoodPicker2.0/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MoodPicker2.0
+//
+//  Created by GREER, PEYTON M. on 4/16/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MoodPicker2.0/MoodPicker2_0App.swift
@@ -0,0 +1,17 @@
+//
+//  MoodPicker2_0App.swift
+//  MoodPicker2.0
+//
+//  Created by GREER, PEYTON M. on 4/16/25.
+//
+
+import SwiftUI
+
+@main
+struct MoodPicker2_0App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
