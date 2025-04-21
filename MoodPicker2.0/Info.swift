0b2f39be1d198d560fc1d715ad070927424f6a83
Peyton Greer <peytongreer73@gmail.com>
2025-04-21 09:48:24 -0500

    Working version just need mood picker to update dashboard

---
+++ MoodPicker2.0/Info.swift
@@ -0,0 +1,18 @@
+//
+//  Info.swift
+//  MoodPicker2.0
+//
+//  Created by GREER, PEYTON M. on 4/21/25.
+//
+
+import SwiftUI
+
+struct Info: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Info()
+}
